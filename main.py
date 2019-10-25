a0d6bfe5bd2e3269b5b8b77d05c9f6da33e82045
season101 <sijan.malla101@gmail.com>
2019-10-24 19:48:34 -0500

    All done with Circles> check for Colors now

--- main.py
+++ main.py
@@ -1,37 +1,41 @@
 #Working with graphics first for program
 from graphics import *
 
-
 def rectangle():
     createRectangle(programWindow, 20,40,40,60,'green',10,'yellow')
 
-def circle():
-    createCircle(programWindow,200,200,100,'red')
+def circle(programWindow):
+
+    for x in range (1,4):
+        for y in range (1,4):
+
+            #Create Circle takes following in order : window, (centre of circle(x,y)), radius, fill color, outline width and color of outline)
+            X=90+x*(75)
+            Y=75+y*(75)
+            createCircle(programWindow,X,Y,75,'',2,"#00ffff")
+
 
 def main():
     #creating graphic window
-    programWindow = createWindow('Graphics Simulator', 891, 630, 'Light Blue')
+    programWindow = createWindow('Graphics Simulator', 500, 670, 'Light Blue')
 
-
-    #Displaying Header for Title
-
-    #Creating TextBox for Title
-    createRectangle(programWindow,290.55,550.34,510.13,590.45,'Blue')
+    #Creating Rectangle To Display Title Box
+    createRectangle(programWindow,120,620,370,650,'Green')
 
     #Creating Text to Display Title
-    createText(programWindow,400,570,"Pattern Creator",16,'bold')
+    createText(programWindow,250,635,"Pattern Creator",16,'bold')
 
+    #Creating Table To Display userChoice
+    createTable(programWindow,'Light Green',1,595,10,'bold',("Patterns","","1. Circles   ","","2. Rectangles",""))
+
+    #Creating Input Window for Taking userChoice
+    createInputBox(programWindow,"Enter pattern Choice (1 or 2): ",460,"Light Green",10,"bold")
 
     #Creating Box to Display Pattern
-    createRectangle(programWindow,222.75,10,668.25,325,'White')
+    createRectangle(programWindow,15,15,485,435,'White')
 
-    #Creating Circle Test (export it to def circle() later after successful execution)
-    for x in range (1,4):
-        for y in range (1,4):
-            #Create Circle takes following in order : window, (centre of circle(x,y)), radius, fill color, outline width and color of outline)
-            X=445.5+x*(100)
-            Y=167.5+y*(100)
-            createCircle(programWindow,X,Y,100,'',2,'Red')
+    circle(programWindow)
+
 
 
     # Waiting for input for program execution.
